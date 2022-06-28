065412414a22c42689e0a64648f0024f667d8cc9
Alexandre Petitjean <petitjean.alexandre.pro@gmail.com>
2022-06-28 02:30:22 +0200

    Add react project

---
+++ ocsi-front/src/App.js
@@ -0,0 +1,12 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className='container bg-base-100 border border-primary mx-auto mt-10'>
+      <h1 className='text-5xl font-bold text-center'>Salut ! </h1>
+    </div>
+  )
+}
+
+
+export default App;
